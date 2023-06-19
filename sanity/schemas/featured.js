c022eaed4341d8f3a5ceae0b4050582c4272ed3d
Phahladira-Moja <phahladira@patala.co.za>
2023-06-20 00:34:35 +0200

    Added sanity and it's client. Also changed the schema for the content

---
+++ sanity/schemas/featured.js
@@ -0,0 +1,28 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'featured',
+  title: 'Featured',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'name',
+      type: 'string',
+      title: 'Name of dish',
+      validation: (Rule) => Rule.required(),
+    }),
+
+    {
+      name: 'short_description',
+      type: 'string',
+      title: 'Short Description',
+      validation: (Rule) => Rule.max(200),
+    },
+    {
+      name: 'restaurants',
+      type: 'array',
+      title: 'Restaurants',
+      of: [{type: 'reference', to: [{type: 'restaurant'}]}],
+    },
+  ],
+})
